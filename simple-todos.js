68d29e6e98483ba3da06917035f8f812d365910a
Julian Samarjiev <julian.samarjiev@commercetools.de>
2015-05-31 22:57:53 +0200

    Tutorial - end of part 5

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,44 @@
+
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      // Show newest tasks first
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   });
+
+  // Inside the if (Meteor.isClient) block, right after Template:body:helpers:
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default from submit
+      return false;
+    }
+  });
+
+  // In the client code, below everything else
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current state
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    // "click.delete": function () {
+    //   Tasks.remove(this._id);
+    // }
+  });
+
 }
